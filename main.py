e3cce66eda6e39dba5ce9f1238beeaf646c6004b
dtkdtkdtk <takadaibsk4126@gmail.com>
2024-07-07 15:46:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,39 +23,13 @@ for i in range(3):
     st.sidebar.markdown(f"- ${i+0.01}")  # 説明のためのダミー
 
 
-def main():
-    llm = ChatOpenAI(temperature=0)
-
+def init_page():
     st.set_page_config(
         page_title="My Great ChatGPT",
         page_icon="🤗"
     )
     st.header("My Great ChatGPT 🤗")
-
-    # チャット履歴の初期化
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
-        ]
-
-    # ユーザーの入力を監視
-    if user_input := st.chat_input("聞きたいことを入力してね！"):
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("ChatGPT is typing ..."):
-            response = llm(st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=response.content))
-
-    # チャット履歴の表示
-    messages = st.session_state.get('messages', [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            with st.chat_message('assistant'):
-                st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message('user'):
-                st.markdown(message.content)
-        else:  # isinstance(message, SystemMessage):
-            st.write(f"System message: {message.content}")
+    st.sidebar.title("Options")
 
 def select_model():
     model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
@@ -89,8 +63,30 @@ def init_messages():
 
 def main():
     init_page()
+
     llm = select_model()
     init_messages()
+
+    messages = st.session_state.get('messages', [])
+    for message in messages:
+        if isinstance(message, AIMessage):
+            with st.chat_message('assistant'):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message('user'):
+                st.markdown(message.content)
+        else:  # isinstance(message, SystemMessage):
+            st.write(f"System message: {message.content}")
+
+    # ユーザーの入力を監視
+    user_input = st.chat_input("聞きたいことを入力してね！")
+    if user_input:
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        st.chat_message("user").markdown(user_input)
+        with st.chat_message("assistant"):
+            st_callback = StreamlitCallbackHandler(st.container())
+            response = llm(messages, callbacks=[st_callback])
+        st.session_state.messages.append(AIMessage(content=response.content))
 
 
 if __name__ == '__main__':
